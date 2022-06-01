37bfb47d42343ecc97bba46c84a16a07b0b8b56c
martimpalmeira <martim.desenvolvedor@gmail.com>
2022-06-01 14:02:12 -0300

    Final

---
+++ js/questions.js
@@ -0,0 +1,132 @@
+// creating an array and passing the number, questions, options, and answers
+let questions = [
+    {
+    numb: 1,
+    question: "Qual o nome do andróide que sempre aparece em todos os filmes?",
+    answer: "C-3PO",
+    options: [
+      "R2-PO",
+      "C-3D2",
+      "C-3PO",
+      "R2-D2"
+    ]
+  },
+    {
+    numb: 2,
+    question: "Qual é a cor do sabre dos Sith?",
+    answer: "Vermelho",
+    options: [
+      "Verde",
+      "Vermelho",
+      "Azul",
+      "Roxo"
+    ]
+  },
+    {
+    numb: 3,
+    question: "O que Luke e Léia são?",
+    answer: "Irmãos",
+    options: [
+      "Irmãos",
+      "Namorados",
+      "Amigos",
+      "Primos"
+    ]
+  },
+    {
+    numb: 4,
+    question: "O que o Luke Skywalker perdeu na luta com Darth Vader?",
+    answer: "Sua mão direita",
+    options: [
+      "Sua mão direita",
+      "Sua perna esquerda",
+      "Sua mão esquerda",
+      "Sua perna direita"
+    ]
+  },
+    {
+    numb: 5,
+    question: "Qual a relação entre Darth Vader e Anakin Skywalker?",
+    answer: "Nenhuma das alternativas",
+    options: [
+      "Pai e filho",
+      "Tio e sobrinho",
+      "Irmãos",
+      "Nenhuma das alternativas"
+    ]
+  },
+  // you can uncomment the below codes and make duplicate as more as you want to add question
+  // but remember you need to give the numb value serialize like 1,2,3,5,6,7,8,9.....
+    {
+    numb: 6,
+    question: "Quais foram as últimas palavras da Padmé?",
+    answer: "Eu sei que ainda há alguma bondade nele",
+    options: [
+      "Você está certo, Obi-Wan",
+      "Tenha certeza que ele nunca descubra",
+      "Encontre e destrua-o",
+      "Eu sei que ainda há alguma bondade nele"
+    ]
+  },
+
+  {
+    numb: 7,
+    question: "Quem libertou a Princesa Leia das correntes no palácio do Jabba?",
+    answer: "R2-D2",
+    options: [
+      "C-3PO",
+      "R2-D2",
+      "Chewbacca",
+      "Luke SkyWalker"
+    ]
+  },
+
+
+  {
+    numb: 8,
+    question: "Qual era a espécie do Yoda?",
+    answer: "Desconhecida",
+    options: [
+      "Desconhecida",
+      "Yangral",
+      "Yolor",
+      "Trilith"
+    ]
+  },
+
+
+  {
+    numb: 9,
+    question: "Quem ensinou Lando Calrissian como pilotar uma nave?",
+    answer: "Han Solo",
+    options: [
+      "Lobot",
+      "Jodelle Foxtrain",
+      "Amaiza Foxtrain",
+      "Han Solo"
+    ]
+  },
+
+
+  {
+    numb: 10,
+    question: "Quantos motores um caça estelar X-wing possui?",
+    answer: "4",
+    options: [
+      "1",
+      "2",
+      "3",
+      "4"
+    ]
+  },
+
+
+
+
+
+
+
+
+
+
+];
